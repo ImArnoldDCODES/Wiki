1bc85b89043763f472a761aa982b41fd197baa57
ImArnoldDCODES <danielsarnold010@gmail.com>
2022-03-09 23:34:38 -0800

    full code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { useState } from 'react';
 
 function App() {
-  const [search, setsearch] = useState("");
-  const [results, setresults] = useState([]);
-  const [searchInfo, setSearch] = useState({});
+  const [search, setSearch] = useState("");
+  const [results, setResults] = useState([]);
+  const [searchInfo, setSearchInfo] = useState({});
 
 
   const handleSearch = async e => {
@@ -13,12 +13,16 @@ function App() {
    const endpoint = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${search}`;
  
    const response = await fetch(endpoint);
-     
-   console.log(response);
-
+      
    if (!response.ok) {
      throw Error(response.statusText)
    }
+   
+   const json = await response.json();
+
+   
+   setResults(json.query.search);
+   setSearchInfo(json.query.searchinfo);
   }
   return (
      <div className="App">
@@ -29,20 +33,33 @@ function App() {
            type="search"
            placeholder="What are you looking for?"
            value={search}
-           onChange={e => setsearch(e.target.value)}
+           onChange={e => setSearch(e.target.value)}
            />
         </form>
-        <p>Search Result: 0</p>
-    </header>
+       { (searchInfo.totalhits) ? <p>Search Results: 
+         {searchInfo.totalhits}</p>
+          : ''}
+
+     </header>
     <div className="results">
-      <div className="result">
-        <h3>Title goes here</h3>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, 
-          adipisicing elit. Aut, accusantium!
-        </p>
-        <a href="#">Read More..</a>
-        </div>
+      {results.map((results, i) =>{
+
+        const url = `https://en.wikipedia.org/?curid=${results.pageid}`
+        ;
+
+        console.log(results.pageid);
+        console.log(url);
+        return (
+          <div className="result" key={i}>
+          <h3>{results.title}</h3>
+
+          <p dangerouslySetInnerHTML=
+          {{ __html: results.snippet}}></p>
+
+          <a href={url}>Read More...</a>
+          </div>
+        )
+      })}
       </div>
     </div>
   )
